ec38569ec12e43c5cf9355aad749313916f0c2d8
stephan <stephan-v@hotmail.com>
2019-12-02 23:15:44 +0100

    Added an initial idea for the landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,62 +6,9 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="https://fonts.googleapis.com/css?family=Calistoga|Roboto&display=swap" rel="stylesheet">
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-67055393-6"></script>
@@ -74,37 +21,30 @@
         </script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div id="app" class="h-100 d-flex align-items-center">
+            <div class="container">
+                <div class="center d-flex">
+                    <div class="row">
+                        <div class="col-md-6 p-5">
+                            <h1 class="mb-5">YourDesignContest<span class="orange">.</span></h1>
+                            <p>We love putting designers and clients in touch with each other.</p>
+                            <p>Join us and become part the next wave of independent designers and freelancers.</p>
+                            <p>Or host a graphic design contest and attract the best designers in our portfolio.</p>
+                        </div><!-- /.col-md-6 -->
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                        <div class="col-md-6 position-relative">
+                            <div class="image-placeholder">
+                                <div class="signup">
+                                    <label for="email">Signup for our launch</label>
+                                    <input type="text" id="email" class="form-control" placeholder="Email Address">
+                                </div><!-- /.signup -->
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+                                <div class="image"></div>
+                            </div><!-- /.image-placeholder -->
+                        </div><!-- /.col-md-6 -->
+                    </div><!-- /.row -->
+                </div><!-- /.modal -->
+            </div><!-- /.container -->
+        </div><!-- /#app -->
     </body>
 </html>
