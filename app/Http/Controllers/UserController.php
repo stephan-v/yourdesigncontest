afd6d2bdbdf69d0faebe2f328116c89bec02eff8
stephan <stephan-v@hotmail.com>
2020-02-03 18:14:41 +0100

    Added the Stripe dashboard button

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\View\View;
+use Stripe\Account;
+use Stripe\Exception\ApiErrorException;
 
 class UserController extends Controller
 {
@@ -12,11 +14,15 @@ class UserController extends Controller
      *
      * @param User $user The user to display.
      * @return View The HTML server response.
+     * @throws ApiErrorException Thrown if the request fails.
      */
     public function show(User $user)
     {
         $user->load('contests.submissions');
 
-        return view('user.show', compact('user'));
+        // The Stripe dashboard for a connected user.
+        $login = Account::createLoginLink($user->stripe_connect_id);
+
+        return view('user.show', ['user' => $user, 'dashboard' => $login->url]);
     }
 }
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -8,13 +8,21 @@
             </div>
         </div>
 
-        <div class="row">
+        <div class="row mb-5">
             <div class="col-md-12">
-                <p>Click here to validation your account for payout</p>
+                <div class="card">
+                    <div class="card-header">Designers only</div>
 
-                <a href="{{ $user->stripeConnectUri }}">
-                    <img src="{{ asset('images/light-on-light.png') }}" alt="">
-                </a>
+                    <div class="card-body">
+                        <p>Click here to validation your account for payout</p>
+
+                        <a href="{{ $user->stripeConnectUri }}">
+                            <img src="{{ asset('images/light-on-light.png') }}" alt="">
+                        </a>
+
+                        <a href="{{ $dashboard }}" class="d-block mt-3 mb-3">Manage your payout settings</a>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -27,7 +35,7 @@
         <div class="row">
             @foreach ($user->contests->flatMap->submissions as $submission)
                 <div class="col-md-3">
-                    <div class="submission ">
+                    <div class="submission mb-3">
                         <div class="d-flex align-content-center justify-content-center p-3">
                             <a href="{{ route('contests.show', ['contest' => $submission->contest]) }}">
                                 <img src="{{ asset($submission->path) }}" alt="" class="img-fluid">
