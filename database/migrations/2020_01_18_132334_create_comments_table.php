9cf8da7a9fc423ffbcbd75404856c7a5db84bf8a
Stephan de Vries <stephan-v@hotmail.com>
2020-02-06 12:17:36 +0100

    Added soft deletes to comments

--- database/migrations/2020_01_18_132334_create_comments_table.php
+++ database/migrations/2020_01_18_132334_create_comments_table.php
@@ -22,6 +22,7 @@ class CreateCommentsTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
 
+            $table->softDeletes();
             $table->timestamps();
         });
     }
