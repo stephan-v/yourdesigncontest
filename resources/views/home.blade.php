7a2d444c44a53e6a66d464b98de5352671456a11
Stephan de Vries <stephan-v@hotmail.com>
2020-03-11 23:00:02 +0100

    Added a new homepage style WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="main-banner home-banner w-100 mb-5">
-        <div class="container d-flex align-items-center h-100">
-            <h1 class="text-white">
-                <div class="mb-3">Quick and cheap creative services</div>
-                <a href="{{ route('process') }}" class="btn btn-primary">Get started.</a>
-            </h1>
-        </div>
-    </div>
+    <div class="position-relative home-header">
+        <img src="{{ asset('images/svg/header.svg') }}" alt="">
 
-    <div class="container">
-        <img src="{{ asset('images/header.jpg') }}" alt="A collaboration of graphic design images" class="img-fluid w-100">
-
-        <div class="bg-white p-4">
-            <h2>Do you need a logo or other graphic design?</h2>
-
-            <p>
-                Creative talent is everywhere. Through our platform we give creatives from all kind
-                of disciplines and backgrounds the change to create engaging content for their favourite brands.
-            </p>
-
-            <p>
-                On our platform we publish graphic design contests which you can join for free. You decide for yourself
-                which contests you want to enter, what you submit and who you work with. By joining contests you gain experience
-                catering to real clients, work on your portfolio and expand your network all while earning cash.
-            </p>
-
-            <p class="m-0">
-                GraphicDesignContests is the place to brand yourself as a creative specialist. Click here to learn more.
-            </p>
+        <div class="container position-absolute">
+            <img src="{{ asset('images/svg/designers.svg') }}" alt="" class="designers">
         </div>
     </div>
 @endsection
