500d6005dd67c702c7ebd2782fdbd1b5e21501b9
Stephan de Vries <stephan-v@hotmail.com>
2020-05-27 21:29:39 +0200

    Added the account creation to the transferwise client

--- app/Domain/Payout/TransferWise.php
+++ app/Domain/Payout/TransferWise.php
@@ -3,6 +3,7 @@
 namespace App\Domain\Payout;
 
 use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
 
 class TransferWise
 {
@@ -32,6 +33,39 @@ class TransferWise
     {
         $response = $this->client->get('profiles');
 
+        return $this->response($response);
+    }
+
+    /**
+     * Send an email for account creation.
+     *
+     * @return mixed
+     */
+    public function accounts()
+    {
+        $response = $this->client->post('accounts', [
+            'json' => [
+                'profile' => 20233,
+                'accountHolderName' => 'Stephan de Vries',
+                'currency' => 'EUR',
+                'type' => 'email',
+                'details' => [
+                    'email' => 'stephan-v@hotmail.com',
+                ]
+            ]
+        ]);
+
+        return $this->response($response);
+    }
+
+    /**
+     * Convert the response to a json decoded associative PHP array.
+     *
+     * @param ResponseInterface $response The server response.
+     * @return mixed The JSON decoded response.
+     */
+    private function response(ResponseInterface $response)
+    {
         return json_decode($response->getBody(), true);
     }
 }
