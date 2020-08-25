32d99000ab68f17ee084cbbfa6798927bdffa614
stephan <stephan-v@hotmail.com>
2020-08-25 20:38:53 +0200

    Updated the start a contest page

--- resources/views/contest/create.blade.php
+++ resources/views/contest/create.blade.php
@@ -1,62 +1,65 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container">
+    <div class="container contact-page d-flex justify-content-center flex-grow-1 flex-column">
         <div class="row pt-5">
-            <div class="col-md-8 offset-md-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h1>Start a design contest</h1>
+            <div class="col-md-5 order-md-1 order-first mb-5">
+                <img src="{{ asset('images/contest.png') }}" alt="" class="img-fluid d-flex m-auto">
+            </div>
 
-                        <form action="{{ route('contests.store') }}" method="POST">
-                            @csrf
-
-                            <div class="form-group">
-                                <label for="name">Contest name</label>
-                                <input type="text" name="name" class="form-control" id="name" placeholder="Name of your contest" value="{{ old('name') }}">
-                                <small class="form-text text-muted">Please provide the name of your design contest.</small>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="name">Contest description</label>
-                                <textarea name="description" class="form-control" id="description" rows="5" placeholder="Briefing of your contest" value="{{ old('description') }}"></textarea>
-                                <small class="form-text text-muted">Please provide a short briefing for your design contest.</small>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="category">Category</label>
-                                <select name="category" class="form-control" id="category">
-                                    <option value="branding" @if (old('branding')) selected @endif>Branding</option>
-                                    <option value="webdesign" @if (old('webdesign')) selected @endif>Webdesign</option>
-                                    <option value="packaging" @if (old('packaging')) selected @endif>Packaging</option>
-                                </select>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="expires_at">Expires in</label>
-                                <select name="expires_at" class="form-control" id="expires_at">
-                                    @for ($i = 1; $i <= 4; $i++)
-                                        <option value="{{ $i }}" @if (old('expires_at') == $i) selected @endif>
-                                            {{ $i }} {{ Str::plural('week', $i) }}
-                                        </option>
-                                    @endfor
-                                </select>
-                            </div>
-
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul class="mb-0">
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-
-                            <button type="submit" class="btn btn-primary">Next</button>
-                        </form>
-                    </div>
+            <div class="col-md-7 order-md-0 order-last">
+                <div class="position-relative">
+                    <div class="dot-pattern"></div>
+                    <h1>Start a contest</h1>
                 </div>
+
+                <p>Designers from around the world review your design brief and submit unique ideas. You provide feedback, hone your favorites and choose a winner.</p>
+
+                <form action="{{ route('contests.store') }}" method="POST">
+                    @csrf
+
+                    <div class="form-group">
+                        <label for="name">Contest name</label>
+                        <input type="text" name="name" class="form-control" id="name" placeholder="Name of your contest" value="{{ old('name') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Contest description</label>
+                        <textarea name="description" class="form-control" id="description" rows="8" placeholder="Briefing of your contest" value="{{ old('description') }}"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="category">Category</label>
+                        <select name="category" class="form-control" id="category">
+                            <option value="branding" @if (old('branding')) selected @endif>Branding</option>
+                            <option value="webdesign" @if (old('webdesign')) selected @endif>Webdesign</option>
+                            <option value="packaging" @if (old('packaging')) selected @endif>Packaging</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <label for="expires_at">Expires in</label>
+                        <select name="expires_at" class="form-control" id="expires_at">
+                            @for ($i = 1; $i <= 4; $i++)
+                                <option value="{{ $i }}" @if (old('expires_at') == $i) selected @endif>
+                                    {{ $i }} {{ Str::plural('week', $i) }}
+                                </option>
+                            @endfor
+                        </select>
+                    </div>
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul class="mb-0">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <button type="submit" class="btn btn-primary w-100">Next</button>
+                </form>
             </div>
         </div>
     </div>
