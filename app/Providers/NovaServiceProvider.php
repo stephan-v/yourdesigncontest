c175a0a070b69441cb91e334ba89e8d3385dde26
stephan <stephan-v@hotmail.com>
2021-03-14 23:55:51 +0100

    Updated the Laravel Nova gate

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'stephan-v@hotmail.com',
+                'stephandevrieschristiaan@gmail.com',
             ]);
         });
     }
